ecf22ad06601848dc892c641318fb13ae8279bab
zendawg <rich@statefive.org>
2014-01-16 17:38:30 +0000

    Renamed columns.

--- migrations/m131010_185759_study_date_time_columns.php
+++ migrations/m131010_185759_study_date_time_columns.php
@@ -4,12 +4,12 @@ class m131010_185759_study_date_time_columns extends CDbMigration
 {
   // Use safeUp/safeDown to do migration with transaction
   public function safeUp() {
-    $this->alterColumn('ophscimagehumphreys_scan_humphrey_xml', 'study_date', 'date');
-    $this->alterColumn('ophscimagehumphreys_scan_humphrey_xml', 'study_time', 'time');
+    $this->alterColumn('ophinvisualfields_humphrey_xml', 'study_date', 'date');
+    $this->alterColumn('ophinvisualfields_humphrey_xml', 'study_time', 'time');
   }
 
   public function safeDown() {
-    $this->alterColumn('ophscimagehumphreys_scan_humphrey_xml', 'study_date', 'varchar(10)');
-    $this->alterColumn('ophscimagehumphreys_scan_humphrey_xml', 'study_time', 'varchar(12)');
+    $this->alterColumn('ophinvisualfields_humphrey_xml', 'study_date', 'varchar(10)');
+    $this->alterColumn('ophinvisualfields_humphrey_xml', 'study_time', 'varchar(12)');
   }
 }
