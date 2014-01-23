ec002b1434b3033554a4292dd34733f672b3af11
zendawg <rich@statefive.org>
2014-01-23 11:28:24 +0000

    Removed code.

--- controllers/DefaultController.php
+++ controllers/DefaultController.php
@@ -18,36 +18,4 @@ class DefaultController extends BaseEventTypeController {
 		parent::actionPrint($id);
 	}
 
-	/**
-	 * Update images for a VF based on test type and strategy.
-	 * 
-	 * @param type $patient_id
-	 * @param type $test_type_id
-	 * @param type $strategy_id
-	 */
-	public function actionUpdateImages($patient_id, $test_type, $strategy) {
-		$doc = new ScannedDocument;
-		if ($doc->isSupported('Visualfields', $test_type)) {
-			$leftImages = $doc->getScannedDocuments('Visualfields', 'humphreys', $patient_id, array('strategy' => $strategy, 'eye' => 'L', 'associated' => 0));
-			$rightImages = $doc->getScannedDocuments('Visualfields', 'humphreys', $patient_id, array('strategy' => $strategy, 'eye' => 'R', 'associated' => 0));
-			$element = Yii::app()->session['_image_element'];
-			$patient = Patient::model()->find('hos_num=\'' . $patient_id . '\'');
-			$this->renderPartial('form_Element_OphInVisualfields_Image', array('element' => $element, 'rightImages' => $rightImages, 'leftImages' => $leftImages, 'patient' => $patient));
-		}
-	}
-
-	/**
-	 * Update images for a VF based on test type and strategy.
-	 * 
-	 * @param type $patient_id
-	 * @param type $test_type_id
-	 * @param type $strategy_id
-	 */
-	public function actionUpdateImage($fileId, $side) {
-		$doc = new ScannedDocument;
-		if ($doc->isSupported('humphreys') && $fileId) {
-			$this->renderPartial('form_Element_OphInVisualfields_Image', array('fileId' => $fileId));
-		}
-	}
-
 }
