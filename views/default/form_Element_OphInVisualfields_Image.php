33722da9d32a601b54e668970931adda189c0d82
Richard Meeking <richard.meeking@openeyes.org.uk>
2014-04-24 15:38:42 +0100

    Removed error code.

--- views/default/form_Element_OphInVisualfields_Image.php
+++ views/default/form_Element_OphInVisualfields_Image.php
@@ -56,11 +56,7 @@ $divName = $element->elementType->class_name;
                             echo 'There are no fields to view for the right eye.';
                         }
                         ?>
-		</div>if (count($right_fields) > 0) {
-                            echo CHtml::activeDropDownList($element, 'right_field_id', CHtml::listData($right_fields, 'cropped_image.id', 'study_datetime'), array('onclick' => 'changeImage(this, "right")'));
-                        } else {
-                            echo 'There are no fields to view for the right eye.';
-                        }
+		</div>
 
 		<div class="side right eventDetail"
 			 data-side="left">
