beecaf0d46383ea354edf2e4540c165b0ecc2cc1
Jamie Neil <jamie.neil@openeyes.org.uk>
2014-05-29 16:47:06 +0100

    [OE-4212] Add result element back into migrations

--- migrations/m140506_161243_visual_fields.php
+++ migrations/m140506_161243_visual_fields.php
@@ -7,6 +7,7 @@ class m140506_161243_visual_fields extends OEMigration
 		$this->createOETable('ophinvisualfields_strategy', array('id' => 'pk', 'name' => 'string not null'), true);
 		$this->createOETable('ophinvisualfields_pattern', array('id' => 'pk', 'name' => 'string not null'), true);
 		$this->createOETable('ophinvisualfields_ability', array('id' => 'pk', 'name' => 'string not null', 'active' => 'boolean not null default true'), true);
+		$this->createOETable('ophinvisualfields_assessment', array('id' => 'pk', 'name' => 'string not null', 'active' => 'boolean not null default true'), true);
 
 		$this->createOETable(
 			'ophinvisualfields_field_measurement',
@@ -69,12 +70,26 @@ class m140506_161243_visual_fields extends OEMigration
 			true
 		);
 
+		$this->createOETable(
+			'et_ophinvisualfields_result',
+			array(
+				'id' => 'pk',
+				'event_id' => 'integer unsigned not null',
+				'assessment_id' => 'integer not null',
+				'other' => 'text',
+				'constraint et_ophinvisualfields_result_event_id_fk foreign key (event_id) references event (id)',
+				'constraint et_ophinvisualfields_result_assessment_id_fk foreign key (assessment_id) references ophinvisualfields_assessment (id)',
+			),
+			true
+		);
+
 		$event_type_id = $this->insertOEEventType('Visual Fields', 'OphInVisualfields', 'In');
 		$this->insertOEElementType(
 			array(
 				'Element_OphInVisualfields_Image' => array('name' => 'Image', 'required' => true),
 				'Element_OphInVisualfields_Condition' => array('name' => 'Condition', 'required' => true),
 				'Element_OphInVisualfields_Comments' => array('name' => 'Comments', 'required' => true),
+				'Element_OphInVisualfields_Result' => array('name' => 'Result', 'required' => true),
 			),
 			$event_type_id
 		);
@@ -99,6 +114,8 @@ class m140506_161243_visual_fields extends OEMigration
 		$this->dropTable('et_ophinvisualfields_condition_version');
 		$this->dropTable('et_ophinvisualfields_comments');
 		$this->dropTable('et_ophinvisualfields_comments_version');
+		$this->dropTable('et_ophinvisualfields_result');
+		$this->dropTable('et_ophinvisualfields_result_version');
 
 		$this->dropTable('ophinvisualfields_field_measurement');
 		$this->dropTable('ophinvisualfields_field_measurement_version');
@@ -109,5 +126,7 @@ class m140506_161243_visual_fields extends OEMigration
 		$this->dropTable('ophinvisualfields_pattern_version');
 		$this->dropTable('ophinvisualfields_ability');
 		$this->dropTable('ophinvisualfields_ability_version');
+		$this->dropTable('ophinvisualfields_assessment');
+		$this->dropTable('ophinvisualfields_assessment_version');
 	}
 }
