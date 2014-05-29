a93e75614f5a9d83b92e660c95925d00370f2d51
Jamie Neil <jamie.neil@openeyes.org.uk>
2014-05-29 15:19:41 +0100

    Fix form wrapper

--- views/default/form_Element_OphInVisualfields_Comments.php
+++ views/default/form_Element_OphInVisualfields_Comments.php
@@ -17,12 +17,6 @@
  * @license http://www.gnu.org/licenses/gpl-3.0.html The GNU General Public License V3.0
  */
 ?>
-<section class="element <?php echo $element->elementType->class_name ?>"
-		 data-element-type-id="<?php echo $element->elementType->id ?>"
-		 data-element-type-class="<?php echo $element->elementType->class_name ?>"
-		 data-element-type-name="<?php echo $element->elementType->name ?>"
-		 data-element-display-order="<?php echo $element->elementType->display_order ?>">
-	<div class="element-fields element-eyes row">
-		<?php echo $form->textArea($element, 'comments', array('rows' => 3, 'cols' => 80)) ?>
-	</div>
-</section>
+<div class="element-fields">
+	<?php echo $form->textArea($element, 'comments', array('rows' => 3, 'cols' => 80)) ?>
+</div>
--- views/default/form_Element_OphInVisualfields_Result.php
+++ views/default/form_Element_OphInVisualfields_Result.php
@@ -17,15 +17,9 @@
  * @license http://www.gnu.org/licenses/gpl-3.0.html The GNU General Public License V3.0
  */
 ?>
-<section class="element <?php echo $element->elementType->class_name ?>"
-		 data-element-type-id="<?php echo $element->elementType->id ?>"
-		 data-element-type-class="<?php echo $element->elementType->class_name ?>"
-		 data-element-type-name="<?php echo $element->elementType->name ?>"
-		 data-element-display-order="<?php echo $element->elementType->display_order ?>">
-	<div class="element-fields element-eyes row">
-		<?= $form->checkBox($element, 'reliability') ?>
-		<?= $form->radioBoolean($element, 'assessment') ?>
-		<?= $form->dropDownList($element, 'assessment_id', CHtml::listData(OphInVisualfields_Assessment::model()->findAll(array('order'=> 'name asc')),'id','name'),array('empty'=>'- Please select -')) ?>
-		<?= $form->textArea($element, 'other', array('rows' => 3, 'cols' => 80), !$element->other) ?>
-	</div>
-</section>
+<div class="element-fields">
+	<?= $form->checkBox($element, 'reliability') ?>
+	<?= $form->radioBoolean($element, 'assessment') ?>
+	<?= $form->dropDownList($element, 'assessment_id', CHtml::listData(OphInVisualfields_Assessment::model()->findAll(array('order'=> 'name asc')),'id','name'),array('empty'=>'- Please select -')) ?>
+	<?= $form->textArea($element, 'other', array('rows' => 3, 'cols' => 80), !$element->other) ?>
+</div>
