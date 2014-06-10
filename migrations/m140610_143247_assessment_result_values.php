eeb86870aa4c27d8a530dc98224834095775412b
Mark Wadham <m@rkw.me>
2014-06-10 14:40:15 +0000

    updated values

--- migrations/m140610_143247_assessment_result_values.php
+++ migrations/m140610_143247_assessment_result_values.php
@@ -4,9 +4,12 @@ class m140610_143247_assessment_result_values extends CDbMigration
 {
 	public function up()
 	{
-		$this->insert('ophinvisualfields_assessment',array('id'=>1,'active'=>1,'name'=>'Wonderful'));
-		$this->insert('ophinvisualfields_assessment',array('id'=>2,'active'=>1,'name'=>'Couldn\'t be better'));
-		$this->insert('ophinvisualfields_assessment',array('id'=>3,'active'=>1,'name'=>'Just great'));
+		$this->insert('ophinvisualfields_assessment',array('id'=>1,'active'=>1,'name'=>'Nasal step'));
+		$this->insert('ophinvisualfields_assessment',array('id'=>2,'active'=>1,'name'=>'Arcuate defect'));
+		$this->insert('ophinvisualfields_assessment',array('id'=>3,'active'=>1,'name'=>'Paracentral defect'));
+		$this->insert('ophinvisualfields_assessment',array('id'=>4,'active'=>1,'name'=>'Hemianopic defect'));
+		$this->insert('ophinvisualfields_assessment',array('id'=>5,'active'=>1,'name'=>'Bitemporal defect'));
+		$this->insert('ophinvisualfields_assessment',array('id'=>6,'active'=>1,'name'=>'Homonymous hemianopia'));
 	}
 
 	public function down()
